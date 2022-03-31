86589961f614b0185757576ba1a242f39373969d
Shane <98501191+codebyshane@users.noreply.github.com>
2022-03-30 22:40:05 -0400

    coding challenge and solution from 30Mar2022

---
+++ battleOf30Mar2022.js
@@ -0,0 +1,16 @@
+//Challenge:
+//This code does not execute properly. Try to figure out why.
+
+
+//  function multiply(a, b){
+//      a * b
+//   }
+
+
+
+//Solution:
+function multiply(a, b){
+    return a * b
+  }
+
+// This code did not execute properly because there was no output command for  a * b.
