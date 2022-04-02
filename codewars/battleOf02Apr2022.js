7a7eff8e2744b163b7f97144ea7a8b66f09cd64f
Shane <98501191+codebyshane@users.noreply.github.com>
2022-04-02 11:09:26 -0400

    coding challenge and solution from 02Apr2022

---
+++ codewars/battleOf02Apr2022.js
@@ -0,0 +1,59 @@
+//Challenge:
+//Messi's Goal Total
+
+//Use variables to find the sum of the goals Messi scored in 3 competitions
+
+//Information
+//Messi goal scoring statistics:
+// Competition	        Goals
+// La Liga	            43
+// Champions League	    10
+// Copa del Rey	        5
+
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+
+
+//PREP
+//Parameters:
+// whole integers > 0 stored in variables
+
+//Results:
+// whole integer > 0; sum of all previous stored integers in variables
+
+//Examples:
+// laLigaGoals = 43
+// championsLeagueGoals = 10
+// copaDelReyGoals = 5
+
+// laLigaGoals + championsLeagueGoals + copaDelReyGoals = totalGoals // 58
+
+// laLigaGoals = 25
+// championsLeagueGoals = 13
+// copaDelReyGoals = 7
+
+// laLigaGoals + championsLeagueGoals + copaDelReyGoals = totalGoals // 45
+
+// laLigaGoals = 40
+// championsLeagueGoals = 8
+// copaDelReyGoals = 12
+
+// laLigaGoals + championsLeagueGoals + copaDelReyGoals = totalGoals // 60
+
+//Solution with Psuedo: 
+
+// declare goals for La Liga in a variable
+var laLigaGoals = 43
+// declare goals for La Liga in a variable
+var championsLeagueGoals = 10
+// declare goals for La Liga in a variable
+var copaDelReyGoals = 5
+
+// create a variable that is equal to the sum of the previous variable values.
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
