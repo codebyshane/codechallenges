55423a767fe9502586783f3181d75e4b0ac31c40
Shane <98501191+codebyshane@users.noreply.github.com>
2022-04-05 14:43:35 -0400

    coding challenge and solution from 05Apr2022

---
+++ codewars/battleOf05Apr2022.js
@@ -0,0 +1,29 @@
+//Challenge:
+//Convert a Number to a String!
+
+//We need a function that can transform a number into a string.
+
+//What ways of achieving this do you know?
+
+
+
+//PREP
+//Parameters:
+//integer
+
+//Results:
+//string
+
+//Examples:
+//123 --> "123"
+//999 --> "999"
+//14000605 --> "14000605"
+
+//Solution with Psuedo:
+
+function numberToString(num) {
+    //use .toString because it is built in and automatically converts the parameter to a string
+    return num.toString();
+  }
+
+numberToString(123)
