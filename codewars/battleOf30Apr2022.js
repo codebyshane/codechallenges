cab72dbfef61a392fa1669f5fc8f3e20ea07e601
Shane <98501191+codebyshane@users.noreply.github.com>
2022-04-30 23:02:08 -0400

    coding challenge and solution from 30Apr2022

---
+++ codewars/battleOf30Apr2022.js
@@ -0,0 +1,36 @@
+// Challenge:
+// c
+
+// Write a simple regex to validate a username. Allowed characters are:
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+
+
+// PREP
+// Parameters:
+// string
+
+// Results:
+// boolean
+
+// Examples:
+// "Hass" --> false
+// "asddsa" --> true
+// "asd43_34" --> true
+
+// Solution with Psuedo: 
+
+function validateUsr(username) {
+    // create a validator that will, from the beginning of the string (^) allow:
+    // characters a-z, numbers 0-9, and underscores ([a-z0-9_])
+    // it will only accept strings between 4 and 16 acceptable characters ({4,16})
+    // end the string after 16 characters ($)
+    const validator = /^[a-z0-9_]{4,16}$/;
+
+    return validator.test(username);
+  }
+
+validateUsr("asd34_43")
