e170b48424332c907964f45c14aae3e705e64ba5
Shane <98501191+codebyshane@users.noreply.github.com>
2022-05-20 23:40:50 -0400

    coding challenge and solution from 20May2022

---
+++ codewars/battleOf20May2022.js
@@ -0,0 +1,31 @@
+// Challenge:
+// Century From Year
+
+// Introduction
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+
+// Given a year, return the century it is in.
+
+
+
+// PREP
+// Parameters:
+//
+
+// Results:
+//
+
+// Examples:
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+
+// Solution with Psuedo: 
+
+function century(year) {
+    // we will use Math.ceil() to round up to the nearest integer
+    return Math.ceil(year/100);
+  }
