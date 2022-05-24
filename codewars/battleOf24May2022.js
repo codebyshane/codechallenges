9ed4cb00a3045e1748a5dd30ea6e8d2a3dbb5860
Shane <98501191+codebyshane@users.noreply.github.com>
2022-05-24 11:10:40 -0400

    coding challenge and solution from 24May2022

---
+++ codewars/battleOf24May2022.js
@@ -0,0 +1,29 @@
+// Challenge:
+// Volume of a Cuboid
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. 
+// Write a function to help Bob with this calculation.
+
+
+
+// PREP
+// Parameters:
+// numbers
+
+// Results:
+// number
+
+// Examples:
+// (3, 3, 3) --> 27
+// (10, 10, 10) --> 1000
+// (1.5, 1.5, 1.5) --> 3.375
+
+
+// Solution with Psuedo: 
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // the formula for volume of a cube is length * width * height
+      return length * width * height;
+    }
+  }
