cb5ccc147f1f49730f182e8f656aca43a2ab8428
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-10 14:58:08 -0400

    code challenge 2022Jun10

---
+++ codewars/8kyu/CountingSheep.js
@@ -0,0 +1,47 @@
+// Challenge:
+// Counting sheep...
+
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+
+// PREP
+// Parameters:
+//
+
+
+// Results:
+//
+
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function countSheeps(arrayOfSheep) {
+    let numberOfSheep = 0
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i] == true) {
+            numberOfSheep++
+        }
+    }
+    return numberOfSheep;
+  }
+
+console.log(countSheeps([true, true, true, false, true, true, true, true, true, false, true, false, true, false, false, true, true, true, true, true, false, false, true, true]))
