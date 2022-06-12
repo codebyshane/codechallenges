4b05dededa9dac87e902a74c493bfc7a8336af24
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-12 15:06:17 -0400

    code challenge 2022Jun12

---
+++ codewars/8kyu/Function1-HelloWorld.js
@@ -0,0 +1,29 @@
+// Challenge:
+// Function 1 - hello world
+
+
+// Description:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+// PREP
+// Parameters:
+// function
+
+// Results:
+// "hello world"
+
+// Examples:
+//
+
+
+// Solution with Psuedo: 
+
+function greet() {
+    return 'hello world!';
+};
