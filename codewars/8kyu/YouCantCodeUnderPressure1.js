fe9bf86795c60afe7a46e79afaf0c2d4cd4aa877
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-13 15:10:17 -0400

    code challenge 2022Jun13

---
+++ codewars/8kyu/YouCantCodeUnderPressure1.js
@@ -0,0 +1,26 @@
+// Challenge:
+// You Can't Code Under Pressure #1
+
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// PREP
+// Parameters:
+// integer
+
+// Results:
+// integer
+
+// Examples:
+// 2 ==> 4
+// 3 ==> 6
+// 8 ==> 16
+
+
+// Solution with Psuedo: 
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return 2 * i;
+  }
