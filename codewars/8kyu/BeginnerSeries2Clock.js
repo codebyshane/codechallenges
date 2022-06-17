2bd8c125f21a24fa8522c8c3110c9c21314e6b15
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-17 15:50:22 -0400

    code challenge 2022Jun17

---
+++ codewars/8kyu/BeginnerSeries2Clock.js
@@ -0,0 +1,42 @@
+// Challenge:
+// Beginner Series #2 Clock
+
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+
+// PREP
+// Parameters:
+// three numbers
+
+// Results:
+// number
+
+// Examples:
+// 0, 1, 1 ==> 61000
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function past(h, m, s){
+    return ((h * 3600000) + (m * 60000) + (s * 1000));
+  }
+
+console.log(past(0, 1, 1));
