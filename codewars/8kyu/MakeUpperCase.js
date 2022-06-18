65eb236697400487afd7cbd387497b8470dc7820
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-18 15:54:24 -0400

    code challenge 2022Jun18

---
+++ codewars/8kyu/MakeUpperCase.js
@@ -0,0 +1,26 @@
+// Challenge:
+// MakeUpperCase
+
+
+// Write a function which converts the input string to uppercase.
+
+
+
+// PREP
+// Parameters:
+// string
+
+// Results:
+// string
+
+// Examples:
+// hello ==> Hello
+// one time at band camp ==> One Time At Band Camp
+// alrighty then ==> Alrighty Then
+
+
+// Solution with Psuedo: 
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+  }
