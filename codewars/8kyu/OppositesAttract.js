269fd81d86e52f692c85b7a2d1d2b57707f9cd7a
Shane <98501191+codebyshane@users.noreply.github.com>
2022-06-21 16:51:48 -0400

    code challenge 2022Jun21

---
+++ codewars/8kyu/OppositesAttract.js
@@ -0,0 +1,31 @@
+// Challenge:
+// Opposites Attract
+
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+
+// PREP
+// Parameters:
+// two numbers
+
+// Results:
+// boolean
+
+// Examples:
+// 1,4 --> true
+// 2,2 --> false
+// 0,1 --> true
+
+
+// Solution with Psuedo: 
+
+function lovefunc(flower1, flower2){
+    if (flower1 % 2 === 0 && flower2 % 2 === 1 || flower1 % 2 === 1 && flower2 % 2 === 0) {
+        return true;
+    } else {
+        return false
+    }
+  }
