ff1e86e1cd8aa90cc6fd54d96a68ff8bf9490169
Shane <98501191+codebyshane@users.noreply.github.com>
2022-07-20 18:31:04 -0400

    code challenge 2022Jul20

---
+++ codewars/8kyu/ParseNiceIntFromCharProblem.js
@@ -0,0 +1,29 @@
+// Challenge:
+// Parse nice int from char problem
+
+
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+
+// PREP
+// Parameters:
+//
+
+// Results:
+//
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function getAge(inputString){
+    return Math.abs(inputString.charAt(0))
+}
