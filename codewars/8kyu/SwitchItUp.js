6a3a0c41a2b4f48dc364585a360866c624141d82
Shane <98501191+codebyshane@users.noreply.github.com>
2022-07-22 19:19:22 -0400

    code challenge 2022Jul22

---
+++ codewars/8kyu/SwitchItUp.js
@@ -0,0 +1,42 @@
+// Challenge:
+// Switch it Up!
+
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+
+// PREP
+// Parameters:
+// number
+
+// Results:
+// string
+
+// Examples:
+// 1 --> "One"
+// 3 --> "Three"
+// 7 --> "Seven"
+
+
+// Solution with Psuedo: 
+
+function switchItUp(number){
+    switch (number) {
+        case 1: return "One"
+        case 2: return "Two"
+        case 3: return "Three"
+        case 4: return "Four"
+        case 5: return "Five"
+        case 6: return "Six"
+        case 7: return "Seven"
+        case 8: return "Eight"
+        case 9: return "Nine"
+        case 0: return "Zero"
+      }
+    }
