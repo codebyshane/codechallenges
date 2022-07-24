f1a20700a19e2b324ac85672bcfce34d3d0c96fc
Shane <98501191+codebyshane@users.noreply.github.com>
2022-07-23 20:05:38 -0400

    code challenge 2022Jul23

---
+++ codewars/8kyu/RemovingElements.js
@@ -0,0 +1,36 @@
+// Challenge:
+// Removing Elements
+
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+// PREP
+// Parameters:
+//
+
+// Results:
+//
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function removeEveryOther(arr){
+    for (let i = 1; i < arr.length; i++) {
+        arr.splice(i, 1);
+    }
+    return arr;
+}
+
+console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]))
