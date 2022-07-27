59538a03a14b2ffcecaf0260ef698f3e9f0ea97f
Shane <98501191+codebyshane@users.noreply.github.com>
2022-07-27 19:39:58 -0400

    code challenge 2022Jul27

---
+++ codewars/8kyu/Function2-SquaringAnArgument.js
@@ -0,0 +1,25 @@
+// Challenge:
+// Function 2 - squaring an argument
+
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// PREP
+// Parameters:
+// number
+
+// Results:
+// number
+
+// Examples:
+// 3 --> 9
+// 2 --> 4
+// 12 --> 144
+
+
+// Solution with Psuedo: 
+
+function square(x){
+    return Math.pow(x, 2);
+}
