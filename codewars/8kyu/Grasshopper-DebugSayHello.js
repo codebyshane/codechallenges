66534fb80ff1b6cb19a83221002b69962216e2ab
Shane <98501191+codebyshane@users.noreply.github.com>
2022-08-04 15:05:13 -0400

    code challenge 2022Aug04

---
+++ codewars/8kyu/Grasshopper-DebugSayHello.js
@@ -0,0 +1,34 @@
+// Challenge:
+// Grasshopper - Debug sayHello
+
+
+// Debugging sayHello function
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+
+// PREP
+// Parameters:
+// string
+
+
+// Results:
+// string
+
+
+// Examples:
+// Spock --> Hello Spock
+// Bones --> Hello Bones
+// Khan --> Hello Khan
+
+
+// Solution with Psuedo: 
+
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
