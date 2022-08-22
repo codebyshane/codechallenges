188911150ef2e8692311eab8e376f2b9cc76992b
Shane <98501191+codebyshane@users.noreply.github.com>
2022-08-22 11:12:16 -0400

    code challenge 2022Aug22

---
+++ codewars/7kyu/Mumbling.js
@@ -0,0 +1,36 @@
+// Challenge:
+// Mumbling
+
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+// PREP
+// Parameters:
+// string
+
+
+// Results:
+// string
+
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+// Solution with Psuedo: 
+
+function accum(s) {
+    return [...s.toUpperCase()].reduce((acc, cur, index) => acc + '-' + cur + cur.toLowerCase().repeat(index))
+}
+
+console.log(accum("abcd"))
