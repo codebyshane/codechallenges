e836d16cf35f4160612f8b8207bf85a5b5d18b05
Shane <98501191+codebyshane@users.noreply.github.com>
2022-09-10 19:15:46 -0400

    code challenge 2022Sep10

---
+++ codewars/7kyu/FindTheNextPerfectSquare.js
@@ -0,0 +1,45 @@
+// Challenge:
+// Find the next perfect square!
+
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+
+// PREP
+// Parameters:
+// number
+
+
+// Results:
+// number
+
+
+// Examples:
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+
+// Solution with Psuedo: 
+
+function findNextSquare(sq) {
+    let square = Math.sqrt(sq);
+
+    if (Number.isInteger(square)) {
+        return Math.pow(square + 1, 2)
+    } else {
+        return -1
+    }
+  }
+
+console.log(findNextSquare(121))
