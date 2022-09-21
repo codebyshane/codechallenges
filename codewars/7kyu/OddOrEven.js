003ec8b201f69b02d3ef6ebac6ab709e46bb3194
Shane <98501191+codebyshane@users.noreply.github.com>
2022-09-20 22:07:59 -0400

    code challenge 2022Sep20

---
+++ codewars/7kyu/OddOrEven.js
@@ -0,0 +1,53 @@
+// Challenge:
+// Odd or Even?
+
+
+// Task:
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+
+// PREP
+// Parameters:
+// array of integers
+
+
+// Results:
+// string
+
+
+// Examples:
+// [0] --> "even"
+// [0, 1, 4] --> "odd"
+// [0, -1, -5] --> "even"
+
+
+// Solution with Psuedo: 
+
+function oddOrEven(array) {
+    if (array.length === 0) {
+        return "even"
+    } else if (array.reduce((a, b) => a + b) % 2 === 0) {
+        return "even"
+    } else {
+        return "odd"
+    }
+ }
+
+console.log(oddOrEven([0, 1, 4]))
