7c9f89f133468480b62e183bdf9fa0b77e0b28ac
Shane <98501191+codebyshane@users.noreply.github.com>
2022-10-31 16:16:35 -0400

    code challenge 2022Oct31

---
+++ codewars/8kyu/ReverseListOrder.js
@@ -0,0 +1,32 @@
+// Challenge:
+// Reverse List Order
+
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+
+// [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+
+// PREP
+// Parameters:
+// array
+
+
+// Results:
+//array
+
+
+// Examples:
+// [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// [9, 2, 0, 7]  -> [7, 0, 2, 9]
+// [1, 2, 3, 4, 5, 6, 7, 8, 9] -> [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+// Solution with Psuedo: 
+
+function reverseList(list) {
+    return list.reverse()
+}
