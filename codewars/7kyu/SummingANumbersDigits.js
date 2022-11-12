5b19348cc37c4dd3a598d586b8d0d61419f88165
Shane <98501191+codebyshane@users.noreply.github.com>
2022-11-11 20:25:32 -0500

    code challenge 2022Nov11

---
+++ codewars/7kyu/SummingANumbersDigits.js
@@ -0,0 +1,37 @@
+// Challenge:
+// Summing a number's digits
+
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+
+
+// PREP
+// Parameters:
+// number
+
+
+// Results:
+// number
+
+
+// Examples:
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+
+
+// Solution with Psuedo: 
+
+function sumDigits(number) {
+    let array =  Array.from(String(Math.abs(number)), Number)
+   return array.reduce((a, b) => a + b)
+}
+
+console.log(sumDigits(-32))
