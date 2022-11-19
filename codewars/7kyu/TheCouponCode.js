3e2c324ec6f1ae5733c360e7e7cb3a5e2d130e6d
Shane <98501191+codebyshane@users.noreply.github.com>
2022-11-18 21:26:42 -0500

    code challenge 2022Nov18

---
+++ codewars/7kyu/TheCouponCode.js
@@ -0,0 +1,45 @@
+// Challenge:
+// The Coupon Code
+
+
+// Story
+
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+// Task
+
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Examples:
+
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+
+// PREP
+// Parameters:
+// 4 strings
+
+
+// Results:
+// boolean
+
+
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+
+// Solution with Psuedo: 
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    let currentTime = Date.parse(currentDate);
+    let expirationTime = Date.parse(expirationDate);
+
+    return (enteredCode === correctCode && currentTime <= expirationTime)
+}
+
+console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015"))
