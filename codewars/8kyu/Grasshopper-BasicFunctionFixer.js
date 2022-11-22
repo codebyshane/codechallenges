c0f63a0d04bdfad5c5a03dee94c96499cf80b128
Shane <98501191+codebyshane@users.noreply.github.com>
2022-11-21 21:57:36 -0500

    code challenge 2022Nov21

---
+++ codewars/8kyu/Grasshopper-BasicFunctionFixer.js
@@ -0,0 +1,32 @@
+// Challenge:
+// Grasshopper - Basic Function Fixer
+
+
+// Fix the function
+
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+
+// PREP
+// Parameters:
+// number
+
+
+// Results:
+// number
+
+
+// Examples:
+// 5 --> 10
+// 10 --> 15
+// 0 --> 5
+
+
+// Solution with Psuedo: 
+
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
