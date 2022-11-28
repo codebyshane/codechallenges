0cd5adea3bb38792b0cce176617ca65f9eaddbd6
Shane <98501191+codebyshane@users.noreply.github.com>
2022-11-27 22:43:03 -0500

    code challenge 2022Nov27

---
+++ codewars/7kyu/SumOfAngles.js
@@ -0,0 +1,28 @@
+// Challenge:
+// Sum of angles
+
+
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+
+// PREP
+// Parameters:
+// number
+
+
+// Results:
+// number
+
+
+// Examples:
+// 3 --> 180
+// 4 --> 360
+// 18 --> 2880
+
+
+// Solution with Psuedo: 
+
+function angle(n) {
+    return ((n - 2) * 180)
+  }
