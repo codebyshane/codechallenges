e90f30a5839552b8d97131e65fa5fa3f156507f5
Shane <98501191+codebyshane@users.noreply.github.com>
2022-12-11 11:48:26 -0500

    code challenge 2022Dec11

---
+++ codewars/8kyu/5WithoutNumbers.js
@@ -0,0 +1,32 @@
+// Challenge:
+// 5 without numbers !!
+
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+// PREP
+// Parameters:
+// 
+
+
+// Results:
+// 
+
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function unusualFive() {
+    let five = "Five!";
+    return five.length
+  }
