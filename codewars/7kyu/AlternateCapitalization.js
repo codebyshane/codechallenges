0ad7e49b4a997e937a0c2347f2b62efafc6a0f93
Shane <98501191+codebyshane@users.noreply.github.com>
2022-12-18 12:49:05 -0500

    code challenge 2022Dec18

---
+++ codewars/7kyu/AlternateCapitalization.js
@@ -0,0 +1,40 @@
+// Challenge:
+// Alternate capitalization
+
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Indexed capitalization
+
+// Even-odd disparity
+
+
+// PREP
+// Parameters:
+// string
+
+
+// Results:
+// array
+
+
+// Examples:
+// "abcdef" --> ['AbCdEf', 'aBcDeF']
+// "codewars" --> ['CoDeWaRs', 'cOdEwArS']
+// abracadabra" --> ['AbRaCaDaBrA', 'aBrAcAdAbRa']
+
+
+// Solution with Psuedo: 
+
+function capitalize(s){
+    return [s.split('').map((v,i)=>i%2==0?v.toUpperCase():v.toLowerCase()).join(''),
+            s.split('').map((v,i)=>i%2!==0?v.toUpperCase():v.toLowerCase()).join('')]
+  };
