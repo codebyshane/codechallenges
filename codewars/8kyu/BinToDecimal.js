d7dad4980546d275defa0a5ddd090d71c6e95955
Shane <98501191+codebyshane@users.noreply.github.com>
2022-12-23 13:16:59 -0500

    code challenge 2022Dec23

---
+++ codewars/8kyu/BinToDecimal.js
@@ -0,0 +1,27 @@
+// Challenge:
+// Bin to Decimal
+
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+// PREP
+// Parameters:
+// string
+
+
+// Results:
+// number
+
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function binToDec(bin){
+    return parseInt(bin, 2)
+  }
