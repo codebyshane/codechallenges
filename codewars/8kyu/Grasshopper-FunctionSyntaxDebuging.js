4debd812ed7db052960e38e27f52e705ef4c7b0b
Shane <98501191+codebyshane@users.noreply.github.com>
2022-12-24 13:19:47 -0500

    code challenge 2022Dec24

---
+++ codewars/8kyu/Grasshopper-FunctionSyntaxDebuging.js
@@ -0,0 +1,27 @@
+// Challenge:
+// Grasshopper - Function syntax debugging
+
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+
+// PREP
+// Parameters:
+//
+
+
+// Results:
+//
+
+
+// Examples:
+// 
+// 
+// 
+
+
+// Solution with Psuedo: 
+
+function main (verb, noun){
+  return verb + noun
+}
