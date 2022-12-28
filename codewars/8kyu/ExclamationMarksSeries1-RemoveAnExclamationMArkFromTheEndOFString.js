8b67f56682d10e4eb00debaa673cdcf38988c9fe
Shane <98501191+codebyshane@users.noreply.github.com>
2022-12-28 13:58:47 -0500

    code challenge 2022Dec28

---
+++ codewars/8kyu/ExclamationMarksSeries1-RemoveAnExclamationMArkFromTheEndOFString.js
@@ -0,0 +1,42 @@
+// Challenge:
+// Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+
+// Description:
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+
+
+// PREP
+// Parameters:
+// string
+
+
+// Results:
+// string
+
+
+// Examples:
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+
+
+// Solution with Psuedo: 
+
+function remove(string){
+    if (string[string.length-1] == '!') {
+        return string.split('').slice(0,string.split('').length-1).join('');
+    } else {
+        return string;
+    }
+}
